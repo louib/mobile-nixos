6f7eb580281746fd985d15678fa65be5dd662f5f
louib <code@louib.net>
2024-10-13 11:02:19 -0400

    chore: bump nix flake inputs

--- pkgs.nix
+++ pkgs.nix
@@ -1,13 +1,7 @@
-let
-  sha256 = "sha256:0g0m7zhpnbgzwn4gmqhjvqd9v6d917p1dg3fk1kwxs2x7v7c1zd4";
-  rev = "d0e1602ddde669d5beb01aec49d71a51937ed7be";
-in
-  builtins.trace "(Using pinned Nixpkgs at ${rev})"
-  import
-  (fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-    inherit sha256;
-  })
-  .defaultNix
-  .inputs
-  .nixpkgs
+import
+(import (fetchTarball "https://github.com/edolstra/flake-compat/archive/master.tar.gz") {
+  src = builtins.fetchGit ./.;
+})
+.defaultNix
+.inputs
+.nixpkgs
